764d679c60d206149b83b45191fa586f0d01efdc
WAJDA9 <wajdizakhama9@gmail.com>
2024-04-28 16:46:31 +0200

    added inverted clipper and home view

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:get_fit/hompage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
